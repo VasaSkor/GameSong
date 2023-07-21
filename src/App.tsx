ab7cd1fa6eaaf5b19bb133f9d55915eef58bc843
VasaSkor <dzho.ker.700@gmail.com>
2023-07-21 08:28:26 +0300

    feat: add test image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from './assets/Logo.svg'
 
 const App = () => {
     return (
         <div>
-          Work
+          <p>Work</p>
+            <img src={logo} alt='logo'/>
         </div>
     );
 };
