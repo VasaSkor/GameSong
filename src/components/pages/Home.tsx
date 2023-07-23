419ae5bf133efd102db44384e03eb3c77d86d35d
VasaSkor <dzho.ker.700@gmail.com>
2023-07-23 11:38:51 +0300

    refactor: rewrote styles for scss

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,8 +5,8 @@ import {NavLink} from "react-router-dom";
 const Home = () => {
     return(
         <div className='home'>
-            <h1 className='home_text'>GAME SOUND QUIZ</h1>
-            <NavLink className='home_link' to='/game' end>
+            <h1 className='home__text'>GAME SOUND QUIZ</h1>
+            <NavLink className='home__link' to='/game' end>
                 Start
             </NavLink>
         </div>
