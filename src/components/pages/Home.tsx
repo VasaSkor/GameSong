c9d734d0b57f3d3792b3c8f17dd4380580d8f19d
VasaSkor <dzho.ker.700@gmail.com>
2023-07-23 11:30:28 +0300

    feat: add the main content of the home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import '../../styles/Home.scss'
+import {NavLink} from "react-router-dom";
 
 const Home = () => {
     return(
-        <div className='home'/>
+        <div className='home'>
+            <h1 className='home_text'>GAME SOUND QUIZ</h1>
+            <NavLink className='home_link' to='/game' end>
+                Start
+            </NavLink>
+        </div>
     )
 };
 
